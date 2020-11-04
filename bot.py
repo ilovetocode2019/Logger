2927f36370dc9a44721583eafd6f76a60ab8d601
Fyssion <fyssioncodes@gmail.com>
2020-11-03 20:19:56 -0600

    Rewrite database queries to use a data_batch
    This reduces the amount of queries to the database significantly.

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import aiohttp
 import asyncio
 import os
 import logging
+import json
 
 import config
 
@@ -16,6 +17,7 @@ logging.basicConfig(
 )
 
 log = logging.getLogger("logger")
+
 
 class Logger(commands.Bot):
     def __init__(self):
@@ -36,8 +38,18 @@ class Logger(commands.Bot):
         log.info("Creating aiohttp session")
         self.session = aiohttp.ClientSession()
 
+        async def init(conn):
+            await conn.set_type_codec(
+                "jsonb",
+                schema="pg_catalog",
+                encoder=json.dumps,
+                decoder=json.loads,
+                format="text",
+            )
+
         log.info("Connecting to database")
-        self.db = await asyncpg.connect(config.database_uri)
+
+        self.db = await asyncpg.create_pool(config.database_uri, init=init)
 
         log.info("Initiating database")
         query = """CREATE TABLE IF NOT EXISTS avatars (
@@ -66,47 +78,121 @@ class Logger(commands.Bot):
         await self.db.execute(query)
 
     async def update_users(self):
+        log.info("Querying user avatar and name changes")
+
         names = await self.db.fetch("SELECT * FROM names;")
         avatars = await self.db.fetch("SELECT * FROM avatars;")
 
+        avatar_batch = []
+        name_batch = []
+
         for user in bot.users:
-            user_avatars = [avatar for avatar in avatars if avatar["user_id"] == user.id]
-            if user.avatar and (not user_avatars or user_avatars[-1]["hash"] != user.avatar):
+            user_avatars = [
+                avatar for avatar in avatars if avatar["user_id"] == user.id
+            ]
+            if user.avatar and (
+                not user_avatars or user_avatars[-1]["hash"] != user.avatar
+            ):
                 filename = f"{user.id}-{user.avatar}.png"
                 await user.avatar_url_as(format="png").save(f"images/{filename}")
 
-                query = """INSERT INTO avatars (user_id, filename, hash)
-                           VALUES ($1, $2, $3);
-                        """
-                await self.db.execute(query, user.id, filename, user.avatar)
+                avatar_batch.append(
+                    {"user_id": user.id, "filename": filename, "hash": user.avatar}
+                )
 
             user_names = [name for name in names if name["user_id"] == user.id]
             if not user_names or user_names[-1]["name"] != user.name:
-                query = """INSERT INTO names (user_id, name)
-                           VALUES ($1, $2);
-                        """
-                await self.db.execute(query, user.id, user.name)
+                name_batch.append({"user_id": user.id, "name": user.name})
+
+        query = """INSERT INTO avatars (user_id, filename, hash)
+                   SELECT x.user_id, x.filename, x.hash
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, filename TEXT, hash TEXT)
+                """
+
+        changed = False
+
+        if avatar_batch:
+            await self.db.execute(query, avatar_batch)
+            total = len(avatar_batch)
+            if total > 1:
+                log.info("Registered %s avatars to the database.", total)
+                changed = True
+
+        if not changed:
+            log.info("No work needed for avatars")
+
+        changed = False
+
+        query = """INSERT INTO names (user_id, name)
+                   SELECT x.user_id, x.name
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, name TEXT)
+                """
+
+        if name_batch:
+            await self.db.execute(query, name_batch)
+            total = len(name_batch)
+            if total > 1:
+                log.info("Registered %s names to the database.", total)
+                changed = True
+
+        if not changed:
+            log.info("No work needed for names")
 
     async def on_ready(self):
+        log.info(f"Logged in as {self.user.name} - {self.user.id}")
+
         log.info("Loading database")
         nicks = await self.db.fetch("SELECT * FROM nicks;")
 
         log.info("Preparing database")
 
+        log.info("Querying member nick changes")
+        nick_batch = []
+
         for member in self.get_all_members():
-            member_nicks = [nick for nick in nicks if nick["user_id"] == member.id and nick["guild_id"] == member.guild.id]
-            if member.nick and (not member_nicks or member_nicks[-1]["nick"] != member.nick):
-                query = """INSERT INTO nicks (user_id, guild_id, nick)
-                           VALUES ($1, $2, $3);
-                        """
-                await self.db.execute(query, member.id, member.guild.id, member.nick)
+            member_nicks = [
+                nick
+                for nick in nicks
+                if nick["user_id"] == member.id and nick["guild_id"] == member.guild.id
+            ]
+            if member.nick and (
+                not member_nicks or member_nicks[-1]["nick"] != member.nick
+            ):
+                nick_batch.append(
+                    {
+                        "user_id": member.id,
+                        "guild_id": member.guild.id,
+                        "nick": member.nick,
+                    }
+                )
+
+        query = """INSERT INTO nicks (user_id, guild_id, nick)
+                   SELECT x.user_id, x.guild_id, x.nick
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, guild_id BIGINT, nick TEXT)
+                """
+
+        changed = False
+
+        if nick_batch:
+            await self.db.execute(query, nick_batch)
+            total = len(nick_batch)
+            if total > 1:
+                log.info("Registered %s nicks to the database.", total)
+                changed = True
+
+        if not changed:
+            log.info("No work needed for nicks")
 
         await self.update_users()
 
-        log.info(f"Logged in as {self.user.name} - {self.user.id}")
+        log.info("Database is now up-to-date")
 
     def run(self):
         super().run(config.token)
+
 
 bot = Logger()
 bot.run()
--- cogs/tracking.py
+++ cogs/tracking.py
@@ -1,40 +1,98 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
+import asyncio
 import logging
+import asyncpg
 
 log = logging.getLogger("logger.tracking")
+
 
 class Tracking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._avatar_batch = []
+        self._name_batch = []
+        self._nick_batch = []
+        self._batch_lock = asyncio.Lock(loop=bot.loop)
+
+        self.bulk_insert_loop.add_exception_type(asyncpg.PostgresConnectionError)
+        self.bulk_insert_loop.start()
+
+    def cog_unload(self):
+        self.bulk_insert_loop.stop()
+
+    async def bulk_insert(self):
+        query = """INSERT INTO avatars (user_id, filename, hash)
+                   SELECT x.user_id, x.filename, x.hash
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, filename TEXT, hash TEXT)
+                """
+
+        if self._avatar_batch:
+            await self.bot.db.execute(query, self._avatar_batch)
+            total = len(self._avatar_batch)
+            if total > 1:
+                log.info("Registered %s avatars to the database.", total)
+            self._avatar_batch.clear()
+
+        query = """INSERT INTO names (user_id, name)
+                   SELECT x.user_id, x.name
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, name TEXT)
+                """
+
+        if self._name_batch:
+            await self.bot.db.execute(query, self._name_batch)
+            total = len(self._name_batch)
+            if total > 1:
+                log.info("Registered %s names to the database.", total)
+            self._name_batch.clear()
+
+        query = """INSERT INTO nicks (user_id, guild_id, nick)
+                   SELECT x.user_id, x.guild_id, x.nick
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, guild_id BIGINT, nick TEXT)
+                """
+
+        if self._nick_batch:
+            await self.bot.db.execute(query, self._nick_batch)
+            total = len(self._nick_batch)
+            if total > 1:
+                log.info("Registered %s nicks to the database.", total)
+
+            self._nick_batch.clear()
+
+    @tasks.loop(seconds=10.0)
+    async def bulk_insert_loop(self):
+        async with self._batch_lock:
+            await self.bulk_insert()
 
     @commands.Cog.listener()
     async def on_user_update(self, before, after):
 
         if before.name != after.name:
-            query = """INSERT INTO names (user_id, name)
-                       VALUSE ($1, $2);
-                    """
-            await self.bot.db.execute(query, after.id, after.name)
+            self._name_batch.append({"user_id": after.id, "name": after.name})
 
         if before.avatar != after.avatar:
             filename = f"{after.id}-{after.avatar}.png"
             await after.avatar_url_as(format="png").save(f"images/{filename}")
 
-            query = """INSERT INTO avatars (user_id, filename, hash)
-                        VALUES ($1, $2, $3);
-                    """
-            await self.bot.db.execute(query, after.id, filename, after.avatar)
+            self._avatar_batch.append(
+                {"user_id": after.id, "filename": filename, "hash": after.avatar}
+            )
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
 
         if before.nick != after.nick:
-            query = """INSERT INTO nicks (user_id, guild_id, nick)
-                       VALUES ($1, $2, $3);
-                    """
-            await self.bot.db.execute(query, after.id, after.guild.id, after.nick)
+            self._nick_batch.append(
+                    {
+                        "user_id": after.id,
+                        "guild_id": after.guild.id,
+                        "nick": after.nick,
+                    }
+                )
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -43,14 +101,37 @@ class Tracking(commands.Cog):
         log.info("Loading database")
         nicks = await self.bot.db.fetch("SELECT * FROM nicks;")
 
+        nick_batch = []
+
         log.info("Updating nicknames")
         for member in guild.members:
-            member_nicks = [nick for nick in nicks if nick["user_id"] == member.id and nick["guild_id"] == member.guild.id]
-            if member.nick and (not member_nicks or member_nicks[-1]["nick"] != member.nick):
-                query = """INSERT INTO nicks (user_id, guild_id, nick)
-                           VALUES ($1, $2, $3);
-                        """
-                await self.bot.db.execute(query, member.id, member.guild.id, member.nick)
+            member_nicks = [
+                nick
+                for nick in nicks
+                if nick["user_id"] == member.id and nick["guild_id"] == member.guild.id
+            ]
+            if member.nick and (
+                not member_nicks or member_nicks[-1]["nick"] != member.nick
+            ):
+                nick_batch.append(
+                    {
+                        "user_id": member.id,
+                        "guild_id": member.guild.id,
+                        "nick": member.nick,
+                    }
+                )
+
+        query = """INSERT INTO nicks (user_id, guild_id, nick)
+                   SELECT x.user_id, x.guild_id, x.nick
+                   FROM jsonb_to_recordset($1::jsonb) AS
+                   x(user_id BIGINT, guild_id BIGINT, nick TEXT)
+                """
+
+        if nick_batch:
+            await self.bot.db.execute(query, nick_batch)
+            total = len(nick_batch)
+            if total > 1:
+                log.info("Registered %s nicks to the database.", total)
 
         log.info("Updating avatars and usernames")
         await self.bot.update_users()
@@ -75,20 +156,23 @@ class Tracking(commands.Cog):
 
         log.info("Updating database")
 
-        if user.avatar and (not user_avatars or user_avatars[-1]["hash"] != user.avatar):
+        if user.avatar and (
+            not user_avatars or user_avatars[-1]["hash"] != user.avatar
+        ):
             filename = f"{user.id}-{user.avatar}.png"
             await user.avatar_url_as(format="png").save(f"images/{filename}")
 
             query = """INSERT INTO avatars (user_id, filename, hash)
                         VALUES ($1, $2, $3);
                     """
-            await self.db.execute(query, user.id, filename, user.avatar)
+            await self.bot.db.execute(query, user.id, filename, user.avatar)
 
         if not user_names or user_names[-1]["name"] != user.name:
             query = """INSERT INTO names (user_id, name)
                         VALUES ($1, $2);
                     """
-            await self.db.execute(query, user.id, user.name)
+            await self.bot.db.execute(query, user.id, user.name)
+
 
 def setup(bot):
     bot.add_cog(Tracking(bot))
