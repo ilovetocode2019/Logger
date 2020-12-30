be5a9652a35613933d3a778c7f010be7e5de93f1
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-12-29 16:44:58 -0800

    Use MinimalHelpCommand instead of DefaultHelpCommand

--- cogs/meta.py
+++ cogs/meta.py
@@ -6,9 +6,19 @@ import traceback
 import datetime
 import humanize
 
+class HelpCommand(commands.MinimalHelpCommand):
+    def get_command_signature(self, command):
+        return "{0.clean_prefix}{1.qualified_name} {1.signature}".format(self, command)
+
 class Meta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._original_help_command = bot.help_command
+        bot.help_command = HelpCommand()
+        bot.help_command.cog = self
+
+    def cog_unload(self):
+        self.bot.help_command = self._original_help_command
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
